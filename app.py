94f884a2e4ec43e301da2e124277b4c067d4e910
Muhammad Atef <96551959+EngMuhammadAtef@users.noreply.github.com>
2024-08-29 03:19:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from scripts import OCR_script
 app = Flask(__name__)
 
 # Enable CORS for all routes or a specific route
-CORS(app, resources={r"/extract_nationalId": {"origins": "https://opi-se.vercel.app"}})
+CORS(app, resources={r"/extract_nationalId": {"origins": "*"}})
 
 # extract nationalId API
 @app.route('/extract_nationalId', methods=['POST'])
